51ab7b3dae3ae2afe02d62ae7a767ccef3abf55d
Admin <admin@Mac-Admin.local>
2017-09-27 16:54:19 +0300

    first

---
+++ Cg_test1/ButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  ButtonView.swift
+//  Cg_test1
+//
+//  Created by Admin on 27.09.17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ButtonView: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
